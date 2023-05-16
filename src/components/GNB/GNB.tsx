3c583463c1d2dbf57019d96277fe26ca5a691312
jjanmo <jjanmo07@hanmail.net>
2023-05-16 21:36:43 +0900

    fix: update gnb style

--- src/components/GNB/GNB.tsx
+++ src/components/GNB/GNB.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import styled from 'styled-components';
+import { useLocation } from '@reach/router';
 
 export default function GNB() {
+  const { pathname } = useLocation();
+
+  const isHome = pathname === '/';
+
   return (
-    <Container>
+    <Container isHome={isHome}>
       <ul>
-        <li>
+        <NavItem active={pathname === '/'}>
           <Link to="/">Home</Link>
-        </li>
-        <li>
+        </NavItem>
+        <NavItem active={pathname === '/blog/'}>
           <Link to="/blog">Blog</Link>
-        </li>
-        <li>
+        </NavItem>
+        <NavItem active={pathname === '/about/'}>
           <Link to="/about">About</Link>
-        </li>
+        </NavItem>
       </ul>
     </Container>
   );
 }
 
-const Container = styled.nav`
+const Container = styled.nav<{ isHome: boolean }>`
   position: fixed;
   top: 0;
   right: 0;
@@ -28,6 +33,11 @@ const Container = styled.nav`
   height: 80px;
   display: flex;
   align-items: center;
+  color: ${({ isHome }) => (isHome ? 'white' : 'black')};
+  background-color: ${({ isHome }) =>
+    isHome ? `rgba(256, 256, 256, 0.2)` : `rgba(200, 200, 200, 0.5)`};
+  box-shadow: 0 5px 20px -10px #000;
+
   ul {
     box-sizing: border-box;
     display: flex;
@@ -37,13 +47,24 @@ const Container = styled.nav`
     width: 100%;
     padding: 0 2rem;
   }
-  li {
-    margin: 0 1rem;
-    font-size: 1.6rem;
+`;
 
-    a {
-      text-decoration: none;
-      color: inherit;
-    }
+const NavItem = styled.li<{ active: boolean }>`
+  position: relative;
+  margin: 0 1rem;
+  font-size: 1.6rem;
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  &::before {
+    position: absolute;
+    bottom: -5px;
+    left: 0;
+    content: '';
+    width: 100%;
+    height: 3px;
+    background-color: ${({ active }) => (active ? '#4834d4' : 'transparent')};
   }
 `;
