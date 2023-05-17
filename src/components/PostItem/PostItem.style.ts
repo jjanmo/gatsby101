71c858d684007d219a39e879d888b1706cbda58f
jjanmo <jjanmo07@hanmail.net>
2023-05-17 20:50:39 +0900

    feat: add bloglist item ui

---
+++ src/components/PostItem/PostItem.style.ts
@@ -0,0 +1,47 @@
+import { Link } from 'gatsby';
+import styled from 'styled-components';
+
+export const ItemLink = styled(Link)`
+  border-radius: 10px;
+  & + & {
+    margin-top: 20px;
+  }
+  &:hover {
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  }
+
+  @media (min-width: 901px) {
+    & + & {
+      margin-top: 0;
+    }
+  }
+`;
+
+export const Container = styled.article`
+  width: 100%;
+  display: flex;
+  align-items: center;
+`;
+export const ImageWrapper = styled.div<{ isEven: boolean }>`
+  flex: 1;
+  order: ${({ isEven }) => (isEven ? 1 : 2)};
+
+  @media (min-width: 901px) {
+    order: 1;
+  }
+`;
+export const Content = styled.div<{ isEven: boolean }>`
+  padding: 10px 20px;
+  flex: 1;
+  align-self: flex-start;
+  order: ${({ isEven }) => (isEven ? 2 : 1)};
+
+  h3 {
+    margin: 0;
+    font-size: 24px;
+  }
+
+  @media (min-width: 901px) {
+    order: 2;
+  }
+`;
---
+++ src/components/PostItem/PostItem.tsx
@@ -0,0 +1,33 @@
+import { ArrayElementType } from '@components/types';
+import { Link } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import React from 'react';
+import * as S from './PostItem.style';
+
+export default function PostItem({
+  excerpt,
+  frontmatter,
+  isEven,
+}: ArrayElementType<Queries.PostsQuery['allMdx']['nodes']> & {
+  isEven: boolean;
+}) {
+  const image = getImage(frontmatter?.image?.childImageSharp?.gatsbyImageData!);
+  return (
+    <S.ItemLink to={`/blog/${frontmatter?.slug}`}>
+      <S.Container>
+        <S.ImageWrapper isEven={isEven}>
+          <GatsbyImage
+            image={image!}
+            alt={frontmatter?.title!}
+            objectFit="contain"
+          />
+        </S.ImageWrapper>
+
+        <S.Content isEven={isEven}>
+          <h3>{frontmatter?.title}</h3>
+          <p>{excerpt}</p>
+        </S.Content>
+      </S.Container>
+    </S.ItemLink>
+  );
+}
