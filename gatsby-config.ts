104795e576edd01e133dbc72f0093e0669b94f78
jjanmo <jjanmo07@hanmail.net>
2023-05-19 19:46:50 +0900

    fix: fix config variable

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -31,7 +31,7 @@ const config: GatsbyConfig = {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_DELIVERY_ACCESS_TOKEN,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
   ],
